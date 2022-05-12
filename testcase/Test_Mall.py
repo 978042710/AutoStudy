fb2f7b5c4c8fcab6b0fe90402fcc31700a6f5a54
v-yibiao <v-yibiao@xiaomi.com>
2022-05-12 16:34:21 +0800

    增加用例

--- testcase/Test_Mall.py
+++ testcase/Test_Mall.py
@@ -1,12 +1,28 @@
 import requests
+from utils.RequestsUtil import requests_get
+from utils.RequestsUtil import requests_post
+from utils.RequestsUtil import Request
+
+
+def list():
+    url = 'https://www.baidu.com'
+    r = requests_get(url)
+    print(r)
+
+
 
 
 def login():
     url = "https://www.moxinhuiyi.com/apis/course/api/course/category/list"
-    r = requests.post(url)
-    print(r.json())
-    print(r.status_code)
+    # r = requests.post(url)
+    # print(r.json())
+    # print(r.status_code)
+    request = Request()
+    r = request.post(url)
+    #r = requests_post(url)
+    print(r)
 
 
 if __name__ == '__main__':
     login()
+    list()
---
+++ utils/RequestsUtil.py
@@ -0,0 +1,57 @@
+import requests
+
+#1.get封装
+def requests_get(url,headers=None):
+    r = requests.get(url,headers = headers)
+    code = r.status_code
+    try:
+        body = r.json()
+    except Exception as e:
+        body = r.text
+
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+    return res
+
+#2.post封装
+def requests_post(url,json=None):
+    r = requests.post(url,json=json)
+    code = r.status_code
+    try:
+        body = r.json()
+    except Exception as e:
+        body = r.text
+
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+    return res
+
+#重构
+class Request:
+    def requests_api(self,url,json=None,headers=None,method='get'):
+        if method == 'get':
+            r = requests_get(url,headers=headers)
+        elif method == 'post':
+            r = requests_post(url,json=json)
+
+        # code = r.status_code
+        # try:
+        #     body = r.json()
+        # except Exception as e:
+        #     body = r.text
+        #
+        # res = dict()
+        # res["code"] = code
+        # res["body"] = body
+        return r
+
+#重构get请求
+    def get(self,url,**kwargs):
+        return self.requests_api(url,method='get',**kwargs)
+
+
+#重构post请求
+    def post(self,url,**kwargs):
+        return self.requests_api(url,method='post', **kwargs)
